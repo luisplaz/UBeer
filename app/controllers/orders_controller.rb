78d1f76faf7fc5a9527bb0575ca7189bf0a46c55
datmar <darius.atmar@gmail.com>
2016-04-16 19:49:03 -0400

    Get beer as hash from hardcoded hash

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,16 +9,15 @@ class OrdersController < ApplicationController
 	    "deliver_at": "15:00:00",
 	    "deliver_to": "114 W. 24th Street, New York, NY 10011",
 	    "customer_id": "1234",
-	    "beer_kind": "Sam Adams", 
+	    "beer_kind": "Budweiser", 
 	    "quantity": "6 packs"
 		}
 
-		query = order_data[:beer_kind].rstrip!.squeeze(" ").gsub!(" ", "%20")
-
-		delivery_api_uri = "https://www.delivery.com/api/data/search?search_type=alcohol&address=1006+Avenue+of+the+Americas,10018&order_time=ASAP&order_type=delivery&client_id=brewhacks2016&keyword=" + query
-
-		if request.xhr?
-
-		end
+		@query = order_data[:beer_kind].rstrip.squeeze(" ").gsub!(" ", "%20")
+		address = "1006+Avenue+of+the+Americas,10018"
+		binding.pry
+		delivery_api_uri = "https://www.delivery.com/api/data/search?search_type=alcohol&section=beer&address=" + address + "&order_time=ASAP&order_type=delivery&client_id=brewhacks2016&keyword=" + @query
+		response = HTTParty.get(delivery_api_uri)
+		beer = response["data"]["products"]
 	end
 end
