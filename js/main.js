460ac477cf9abc27e3617b2abd518486f074f1d3
luisplaz <luisplaz@hotmail.com>
2016-04-16 21:06:57 -0400

    Fix frontend

--- js/main.js
+++ js/main.js
@@ -7,29 +7,54 @@ $(document).ready(function() {
 
 		setTimeout(function(){
 			$("#start_button").remove();
-			$(".form-group").append('<h1 class="grey">Your Order</h1><br>\
-			<form>\
-			        <label for="name">Name: </label>\
-			        <input id="name" type="text" name="fname" value="David Banner"><br>\
-			        <label for="address">Address: </label>\
-  					<input id="address" type="text" name="fname" value="212 East 63 Street"><br>\
-  					<label for="order">Order: </label>\
-  					<input id="order" type="text" name="fname" value="Budweiser"><br>\
-  					<label for="count">Count: </label>\
-  					<input id="count" type="text" name="fname" value="6"><br><br><br>\
-  					<img src="img/budweiser-6.jpg" class="cart-img"><br><br>\
-  					<label for="eta">ETA: </label>\
-  					<input id="eta" type="text" name="fname" value="10 Minutes"><br>\
-  					<label for="total">Total: </label>\
-  					<input id="total" type="text" name="fname" value="$13.99"><br><br>\
-  					<label for="end"></label>\
-  					<input id="end" type="submit" value="CONFIRM">\
-			</form>\
+			$(".form-group").append('<div class="container">\
+				<h2>ORDER INFORMATION</h2>\
+  				<img src="img/budweiser-6.jpg" class="cart-img"><br><br>\
+				<form class="form-horizontal" role="form">\
+				<div class="form-group">\
+				<label class="control-label col-sm-2" for="name">Full Name</label>\
+				<div class="col-sm-10">\
+				<input type="text" class="form-control" id="name" value="Adam Samuels">\
+				</div>\
+				</div>\
+				<div class="form-group">\
+				<label class="control-label col-sm-2" for="address">Adress</label>\
+				<div class="col-sm-10">\
+				<input type="text" class="form-control" id="address" value="212 East 63 Street">\
+				</div>\
+				</div>\
+				<div class="form-group">\
+				<label class="control-label col-sm-2" for="order">Order</label>\
+				<div class="col-sm-10">\
+				<input type="text" class="form-control" id="order" value="Budweiser">\
+				</div>\
+				</div>\
+				<div class="form-group">\
+				<label class="control-label col-sm-2" for="quantity">Quantity</label>\
+				<div class="col-sm-10">\
+				<input type="text" class="form-control" id="quantity" value="6 Pack">\
+				</div>\
+				</div>\
+				<div class="form-group">\
+				<label class="control-label col-sm-2" for="eta">ETA</label>\
+				<div class="col-sm-10">\
+				<input type="text" class="form-control" id="eta" value="10 Minutes">\
+				</div>\
+				</div>\
+				<div class="form-group">\
+				<label class="control-label col-sm-2" for="total">Total</label>\
+				<div class="col-sm-10">\
+				<input type="text" class="form-control" id="total" value="$13.99">\
+				</div>\
+				</div>\
+				<div class="form-group">\
+				<div class="col-sm-offset-2 col-sm-10">\
+				<button type="submit" class="btn btn-default">Submit</button>\
+				</div>\
+				</div>\
+				</form>\
+				</div>\
 			')
 		},2000);
-
-		
-
-
 	});
 });
