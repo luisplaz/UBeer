3f5e5c6481554693dd387c83f4a85e7843b860bc
luisplaz <luisplaz@hotmail.com>
2016-04-17 12:33:55 -0400

    Add Dynamic address for google maps

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,19 @@
+var deliveryAddress = null;
+var comerceAddress = null;
+
 $(document).ready(function() {
 	$("#start_button").click(function(e){
 		e.preventDefault();
+		
 		var beer = new Audio("/assets/open_beer.wav");
+		var confirm = new Audio("/assets/confirm.ogg");
 		$(".beer").toggleClass("rotate");
-		beer.play()
+		beer.play();
 		var price = null;
 		var estimate = null;
 		var merchantName = null;
 		var quantity = null;
 		var productName = null;
-		var deliveryAddress = null;
 		var username = null;
 		var image = null;
 
@@ -22,6 +26,7 @@ $(document).ready(function() {
       	price = response.price;
       	estimate = response.merchants[0].ordering.availability.delivery_estimate;
       	merchantName = response.merchants[0].summary.name;
+      	comerceAddress = response.merchants[0].location.street;
       	quantity = response.quantity;
       	productName = response.beer.name;
       	deliveryAddress = response.delivery_address;
@@ -32,7 +37,7 @@ $(document).ready(function() {
 		setTimeout(function(){
 			$("#start_button").remove();
 			$(".example").remove();
-			$(".form-group").append('<div class="container">\
+			$(".form-group").append('<div class="container receipt">\
 				<h2>ORDER INFORMATION</h2>\
   				<img src="'+image+'" class="cart-img"><br><br>\
 				<form class="form-horizontal" role="form">\
@@ -79,13 +84,40 @@ $(document).ready(function() {
 				</div>\
 				</div>\
 				<div class="form-group">\
+				<label class="control-label col-sm-2" for="delivery-asap">ASAP Delivery</label>\
+				<div class="col-sm-8">\
+				<input type="checkbox" class="form-control" id="delivery-asap">\
+				</div>\
+				</div>\
+				<div class="form-group">\
+				<label class="control-label col-sm-2" for="delivery-batch">Reduced Price Delivery (+ 5-10 min)</label>\
+				<div class="col-sm-8">\
+				<input type="checkbox" class="form-control" id="delivery-batch">\
+				</div>\
+				</div>\
+				<div class="form-group">\
 				<div class="col-sm-offset-2 col-sm-8">\
-				<button type="submit" class="btn btn-default">Submit</button>\
+				<button type="submit" class="btn btn-default order-button">Submit</button>\
 				</div>\
 				</div>\
 				</form>\
 				</div>\
 			')
+				confirm.play();
 		},7000);
 	});
+
+	$(".form-group").on("click",".order-button",function(e){
+		e.preventDefault();
+		    var Url = "https://www.google.com/maps/embed/v1/directions?";
+            var origin = "origin=".concat(comerceAddress).concat("&");
+            var destination = "destination=".concat(deliveryAddress).concat("&");
+            var mode = "mode=".concat("driving").concat("&");
+            var key = "key=".concat("AIzaSyAVmLyfYUxUNkp_v32R3uo1WpcIPijXeVo");
+            Url = Url.concat( origin, destination,mode,key);
+		$(".receipt").remove()
+		$(".jumbotron").append('<iframe width="600" height="450" frameborder="0" style="border:0" src="'+Url+'" allowfullscreen>\
+			</iframe>\
+				')
+		});
 });
